76a92a9b3db0b629f62556629b58bdd0b80145e8
Skater000333 <pkhungar6@gmail.com>
2025-07-29 20:22:20 +0530

    layout toggle

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,14 @@ import { motion } from "framer-motion";
 import FunFactsCarousel from "./FunFactsCarousel";
 import SkillsBar from "./SkillsBar";
 import ProjectsSection from "./ProjectsSection";
+import ThemeToggle from "./ThemeToggle";
+
 
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-white dark:bg-black">
+      <ThemeToggle />
       <main className="flex flex-col gap-8 row-start-2 items-center">
         {/* Animated Hero Section */}
         <motion.div
