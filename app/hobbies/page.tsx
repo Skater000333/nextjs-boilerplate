234ab1dd8211a8b26d927012599ee5c30bba0ef4
Skater000333 <pkhungar6@gmail.com>
2025-07-30 17:03:58 +0530

    pages added1

---
+++ app/hobbies/page.tsx
@@ -0,0 +1,10 @@
+export default function Hobbies() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center px-4 py-12">
+      <h1 className="text-3xl font-bold mb-4 text-blue-700">Hobbies</h1>
+      <p className="text-lg text-gray-700 text-center">
+        This is where I’ll share more about my hobbies soon!
+      </p>
+    </div>
+  );
+}
